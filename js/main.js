f8f9ff0d6c260dbe1fcb62bb86355f9c5514fea6
nikjke <nikjke@mail.ru>
2021-04-08 23:09:43 +0300

    edit slider

--- js/main.js
+++ js/main.js
@@ -80,17 +80,50 @@ $(document).ready(function() {
     ]
   });
 });
-
+/*
 $(document).ready(function() {
-  $('.slick-slider3').slick({
-    arrows: false,
-    slidesToShow: 2,
+  $('.slick-slider-4').slick({
+    arrows: true,
+    slidesToShow: 3.4,
+    infinite: false,
     responsive: [
       {
         breakpoint: 992,
         settings: {
           arrows: false,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
           slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+*/
+
+$(document).ready(function() {
+  $('.slick-slider-4').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    centerPadding: '120px',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          slidesToShow: 3
         }
       },
       {
